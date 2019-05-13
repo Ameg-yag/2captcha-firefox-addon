9c66d038aa5f1860962d33898c5545a8dc0b092b
Yasin <email@yasinyilmaz.net.tr>
2019-05-13 16:27:06 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# About 
+
+Cybercriminals bypass captcha using this method. 
+
+## Solve Captcha Automatically
+
+### How does that happens? 
+
+User uploads a captcha to 2Captcha service 
+
+2Captcha give an unique ID to the captcha, send this ID to customer and charge his balance for solving the captcha
+
+When user requests the answer by ID of captcha, 2Captcha return the answer. 
+
+### Get API
+
+Get your API key from your account settings page. It's that looks like:
+1abc234de56fab7c89012d34e56fa7b8
+
+![API KEY](https://raw.githubusercontent.com/yasinyilmaz/2captcha-firefox-addon/master/README-img/2capctcha-api.png?token=AJAW6PYRSC3X4LVBRJTEAFS44KUW4)
+
+### How to use this usigned add-on?
+
+This extension unsigned. Firefox will allow you to override the setting to enforce the extension signing requirement, by changing the preference xpinstall.signatures.required and extensions.langpacks.signatures.required to false in the Firefox Configuration Editor (about:config page).
+
+### Screenshot
+
+![Add-on Options](https://raw.githubusercontent.com/yasinyilmaz/2captcha-firefox-addon/master/README-img/2capctcha-firefox-addon.png?token=AJAW6P2UXB4HJPMUGFUC2IK44KUVS)
+
+
+# Credit: @nowon48 (Extension created by this person. Thanks!) 
+
+Original repository (source code): https://github.com/nowon48/2Captcha-ReCaptcha-Solver
---
+++ source-code/content/main.js
@@ -0,0 +1,154 @@
+let pageurl = window.location.href;
+let messageError = "ERROR_WRONG_USER_KEY";
+let messageNotReady = "CAPCHA_NOT_READY";
+let id = "";
+let message = document.createElement('span');
+let afterElement = null;
+
+function checkCompletion(code, key, repeat) {
+	setupTextarea();
+	let http = new XMLHttpRequest();
+	let url = `https://2captcha.com/res.php?key=${key}&action=get&id=${code}&json=0`;
+	http.onreadystatechange = function() {
+		if(http.readyState == 4 && http.status == 200){
+			let response = http.responseText;
+			if(response == messageNotReady){
+				setTimeout(function() {
+					checkCompletion(code, key, repeat);
+				}, repeat)
+			}else{
+				setCaptchaCode(response.substring(3));
+			}
+		}
+	}
+	http.open("GET", url, true);
+	http.send(null);
+}
+
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function insertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+
+function setChecked(interval){
+	let check = document.createElement("img");
+	check.src = browser.extension.getURL("./icons/check.png");
+	check.style.marginTop = "-60px";
+	check.style.marginLeft = "5px";
+	check.style.position = "absolute";
+	insertAfter(check, afterElement);
+	alert(`${pageurl} adresindeki captcha çözüldü!`);
+}
+
+function setCaptchaCode(code) {
+	let ele = document.getElementById("g-recaptcha-response");
+	sendMessage("<b>Captcha çözüldü!</b>");
+	setChecked();
+	if(ele != null){
+		// uncomment this to show the textarea
+		ele.style.display = "block";
+		ele.innerHTML = code;
+	}
+}
+
+function setupTextarea() {
+	let ele = document.getElementById("g-recaptcha-response");
+	if(ele != null){
+		// ele.style.display = "inline-block";
+		// ele.style.zIndex = "-1";
+	}
+}
+
+function setupMessageBox() {
+	let image = '<img src="' + browser.extension.getURL('./icons/icon.png') + '" align="left" style="margin-right: 4px;" />';
+	message.innerHTML = "<b>Captcha çözülüyor...</b>";
+	let container = document.createElement('div');
+	container.className = '2captcha_solver';
+	container.innerHTML = image;
+	container.appendChild(message);
+	container.style.backgroundColor = "#F9F9F9";
+	container.style.border = "1px solid #D3D3D3";
+	container.style.borderTop = "none";
+	container.style.borderRadius = "0 0 3px 3px";
+	container.style.padding = "5px";
+	container.style.boxSizing = "border-box"
+	container.style.width = "302px";
+	container.style.margin = "-4px 2px 0 0";
+
+	insertAfter(container, afterElement);
+	// afterElement.appendChild(message);
+}
+
+function sendMessage(str){
+	message.innerHTML = str;
+}
+
+function startWatching(code, key) {
+	let initial = 15000, repeat = 5000;
+	setTimeout(function() {
+		checkCompletion(code, key, repeat);
+	}, initial);
+}
+
+function makeRequest(result){
+	setupTextarea();
+	sendMessage("<b>Captcha çözülüyor...</b>");
+	let key = result.apiKey;
+	let url = `https://2captcha.com/in.php?key=${key}&googlekey=${id}&method=userrecaptcha&pageurl=${pageurl}`;
+	let http = new XMLHttpRequest();
+	http.onreadystatechange = function() {
+		if(http.readyState == 4 && http.status == 200){
+			let response = http.responseText;
+			if(response == messageError){
+				sendMessage("Sağlanan API anahtarı hatalı.")
+			}else{
+				startWatching(response.substring(3), key);
+			}
+		}else if(http.readyState == 4){
+			sendMessage("2captcha.com tarafında hata meydana geldi!");
+		}
+	}
+	http.open("GET", url, true);
+	http.send(null);
+}
+
+
+
+browser.storage.local.get("isEnabled").then(function(result) {
+	if(result.isEnabled){
+		// wait till id is visible
+		let checkId = setInterval(function() {
+			let ele = document.getElementsByClassName('g-recaptcha');
+			if(ele != null && ele[0] != undefined){
+				id = ele[0].getAttribute('data-sitekey');
+				afterElement = ele[0].firstElementChild
+			}else{
+				let frames = document.getElementsByTagName("iframe");
+				for(let i = 0; i < frames.length; i++){
+					let src = frames[i].getAttribute('src');
+					if(src != null && src.startsWith("https://www.google.com/recaptcha")){
+						id = getParameterByName("k", src);
+						if(id != "" && id != null){
+							afterElement = frames[i];
+							break;
+						}
+					}
+				}
+			}
+			if(id != "" && id != null){
+				setupMessageBox();
+				clearInterval(checkId);
+				browser.storage.local.get("apiKey").then(makeRequest);
+			}
+		}, 1000);
+	}
+});
